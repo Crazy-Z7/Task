f4de5f44df61af6654cf6f3a2efeef87a25e92c7
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2023-07-23 09:48:23 +0800

    更新 Glados.js

--- Glados.js
+++ Glados.js
@@ -13,7 +13,7 @@ hostname = glados.rocks
 const cookieName = "Glados"
 const signurlKey = 'photonmang_signurl_glados'
 const signheaderKey = 'photonmang_signheader_glados'
-const photonmang = Env()
+const photonmang = Env('Glados')
 const signurlVal = photonmang.getdata(signurlKey)
 const signheaderVal = photonmang.getdata(signheaderKey)
 sign()  
