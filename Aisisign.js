657b8315aed746278e99f7b9153037fdadcecade
Zoo <110404406+Crazy-Z7@users.noreply.github.com>
2023-07-26 08:37:54 +0800

    更新 Aisisign.js

--- Aisisign.js
+++ Aisisign.js
@@ -30,7 +30,6 @@ function sign() {
     if (result.code == 0) {
       subTitle = `签到结果: 签到成功`
       detail += `积分增加:${result.data.score}`+`\n`
-      detail += `已连续签到:${result.data.type}`+`/7天`
       detail += `积分总数:${result.data.totalscore}`+`\n`
     } else  {
       subTitle = `签到结果: 签到失败`
